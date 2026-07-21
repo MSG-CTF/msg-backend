74e0cb3fed8f99dec6f487966e3cc973bb930539
junha <junha020607@gmail.com>
2026-07-21 15:50:50 +0900

    feat: 앱 구조 설정 apps/ 하위로

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'apps.accounts',
+    'apps.teams',
+    'apps.adminpanel',
+    'apps.common'
 ]
 
 MIDDLEWARE = [
