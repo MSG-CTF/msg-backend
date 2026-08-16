ed1f5cf267708613eca71890855211f6cc531f7d
김가연 <y@rkdusy.local>
2026-08-16 14:20:57 +0900

    DATETIME_FORMAT 초 단위로 설정

--- config/settings.py
+++ config/settings.py
@@ -128,3 +128,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DATETIME_FORMAT": "%Y-%m-%dT%H:%M:%SZ",
+}
