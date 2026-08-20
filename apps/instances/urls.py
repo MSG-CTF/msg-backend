807057ed491278a2293713b226f260c126685e51
gyumin <gimgyumin0207@gmail.com>
2026-08-20 20:04:12 +0900

    fix: 내 인스턴스 조회 URL 등록

--- apps/instances/urls.py
+++ apps/instances/urls.py
@@ -5,6 +5,7 @@ from apps.instances.views import (
     InstanceDeleteView,
     InstanceExtendView,
     InstanceResetView,
+    MyInstanceView,
 )
 
 urlpatterns = [
@@ -12,4 +13,5 @@ urlpatterns = [
     path("instances/<uuid:instance_id>", InstanceDeleteView.as_view(), name="instance-delete"),
     path("instances/<uuid:instance_id>/reset", InstanceResetView.as_view(), name="instance-reset"),
     path("instances/<uuid:instance_id>/extend", InstanceExtendView.as_view(), name="instance-extend"),
+    path("teams/me/instance", MyInstanceView.as_view(), name="my-instance"),
 ]
